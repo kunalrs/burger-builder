766d5ef8609873dfcbaeb8554e4dc8b5681e8562
Kunal <sagar.kunal@yahoo.com>
2020-04-07 08:57:49 +0530

    Logo added

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerLogo from '../../assets/images/logo.png';
+import classes from './Logo.module.css';
+
+const Logo = (props) => {
+    return <div className={classes.Logo}>
+        <img src={BurgerLogo} alt="Burger" />
+    </div>
+};
+
+export default Logo;
