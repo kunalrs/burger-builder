2fafe7cdedf2b31682a28503a4211986f147bed9
Kunal <sagar.kunal@yahoo.com>
2020-04-09 16:49:20 +0530

    added logic to dismiss sidedrawer and backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = (props) => {
+
+    const [showSideDrawer, setSideDrawerShowState] = useState(true);
+
+    const sideDrawerClosedHandler = () => {
+       setSideDrawerShowState(false);
+    }
+
     return (
         <Aux>
             <Toolbar />
-            <SideDrawer />
+            <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
             <main className={classes.Content}>
                 {props.children}
             </main>
