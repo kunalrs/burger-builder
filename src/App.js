f953bd44aeace6119802de9567d37f386adbd46f
Kunal <sagar.kunal@yahoo.com>
2020-04-10 08:41:48 +0530

    Project restructed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './containers/Layout/Layout';
 import BurderBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
